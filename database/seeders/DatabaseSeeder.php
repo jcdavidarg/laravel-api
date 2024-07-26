7e165e257238a3cbfc5d4b9a46a1782b9edd9eb8
jcdavidarg <jcdavidarg@gmail.com>
2024-07-26 19:38:14 -0300

    created a new "tags" method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create(['name' => 'i@admin.com']);
+        User::factory()->create(['email' => 'i@admin.com']); //  ACA ESPECIFICO
+        User::factory(29)->create();    //  ACA ALEATOREAMENTE 
+
+        Category::factory(12)->create();
+        Recipe::factory(100)->create();
+        Tag::factory(40)->create();
+
+        //  RELACION MUCHOS A MUCHOS
+
+        $recipes = Recipe::all();
+        $tags = Tag::all();
+
+        foreach($recipes as $recipe){
+            $recipe->tags()->attach($tags->random(rand(2, 4)));
+        }
     }
 }
